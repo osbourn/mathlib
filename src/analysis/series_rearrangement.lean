185188be57f5429cbbcf7274eb56d542f84ee914
Niels Voss <code@nielsvoss.com>
2023-02-25 12:20:37 -0800

    create and prove sums_gt_M

--- src/analysis/series_rearrangement.lean
+++ src/analysis/series_rearrangement.lean
@@ -32,16 +32,35 @@ lemma neg_exceeds_value (hc : converges a) (hca : ¬converges_absolutely a) (M :
 
 noncomputable def pos_least_exceeding (M : ℝ) : ℕ := nat.find (pos_exceeds_value hc hca M)
 
-noncomputable def neg_least_exceeding (M : ℝ) : ℕ := nat.find (neg_exceeds_value hc hca M)
+noncomputable def neg_least_below (M : ℝ) : ℕ := nat.find (neg_exceeds_value hc hca M)
+
+lemma pos_least_exceeding_spec (M : ℝ) : M < (finset.range $ pos_least_exceeding hc hca M).sum (pos_terms a) :=
+nat.find_spec (pos_exceeds_value hc hca M)
 
 noncomputable def gen_bounds (M : ℝ) : ℕ → (ℕ × ℕ)
 | 0 := let p₁ : ℕ := pos_least_exceeding hc hca M in
-            let q₂ : ℕ := neg_least_exceeding hc hca (M - (finset.range p₁).sum (pos_terms a)) in
-            (p₁, q₂)
-| (step + 1) := let (p_prev, q_prev) := gen_bounds step in
-                let pₙ := pos_least_exceeding hc hca (M - (finset.range p_prev).sum (pos_terms a) - (finset.range q_prev).sum (neg_terms a)) in
-                let qₙ := neg_least_exceeding hc hca (M - (finset.range pₙ).sum (pos_terms a) - (finset.range q_prev).sum (neg_terms a)) in
+       let q₂ : ℕ := neg_least_below hc hca (M - (finset.range p₁).sum (pos_terms a)) in
+       (p₁, q₂)
+| (step + 1) := let pₙ := pos_least_exceeding hc hca (M - (finset.range (gen_bounds step).snd).sum (neg_terms a)) in
+                let qₙ := neg_least_below hc hca (M - (finset.range pₙ).sum (pos_terms a)) in
                 (pₙ, qₙ)
+
+noncomputable def p_bound (M : ℝ) (step : ℕ) : ℕ := (gen_bounds hc hca M step).fst
+noncomputable def q_bound (M : ℝ) (step : ℕ) : ℕ := (gen_bounds hc hca M step).snd
+
+lemma sums_gt_M (M : ℝ) (step : ℕ) : M < (finset.range (p_bound hc hca M (step + 1))).sum (pos_terms a) +
+  (finset.range (q_bound hc hca M step)).sum (neg_terms a) :=
+begin
+  unfold p_bound,
+  unfold gen_bounds,
+  change M < (finset.range
+      (pos_least_exceeding hc hca (M - (finset.range (q_bound hc hca M step)).sum (neg_terms a)))).sum
+        (λ (x : ℕ), pos_terms a x) +
+      (finset.range (q_bound hc hca M step)).sum (neg_terms a),
+  have := pos_least_exceeding_spec hc hca (M - (finset.range (q_bound hc hca M step)).sum (neg_terms a)),
+  have := add_lt_add_right this ((finset.range $ q_bound hc hca M step).sum (neg_terms a)),
+  simpa using this,
+end
 
 theorem riemann_rearrangement_theorem {series : ℕ → ℝ} (h₁ : converges series)
   (h₂ : ¬converges_absolutely series) (C : ℝ) : ∃ (p : ℕ → ℕ), function.bijective p ∧
